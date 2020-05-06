3d986e32475820661655de806200a88ec52f3f0d
arjellel <52532423+arjellel@users.noreply.github.com>
2020-05-07 00:25:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -21,178 +21,95 @@ bot.on('message', message=>{
     
     // const command = args.shift().toLowerCase()
 
-// if(message.content.startsWith('!link')){
-    
-//    let embed = new  Discord.RichEmbed()
-//    embed.addField("roskau endrrimtar", "Join: [link](https://discord.gg/tQcdvq)")
-
-//    message.channel.send(embed)
-    
-  
-// }
-
-// msg.channel.fetchMessages.then(msgs => {const msgstodelete = lol }
-
-
-
 
 var msg = message
 
 var args = message.content.slice(PREFIX.length).split(/ +/g)
 const command = args.shift().toLowerCase()
 
-if(message.content.toLowerCase().startsWith === '!setStatus'){
-     if(!message.author.hasPermission('ADMINISTRATOR')){
-    return;
-  }
- db.set(`bot_status`, args)
-  
 
- message.channel.send('Done! Status Changed')
- return; 
+////////////////////////////////
+if(message.content === '!setChannel'){
+  if(!message.member.hasPermission('ADMINISTRATOR')){
+ return;
 }
+db.set(`id_${message.guild.id}`, message.channel.id)
+return message.channel.send('done');
 
 
 
-if(message.content.toLowerCase() === '!setChannel'){
-     if(!message.author.hasPermission('ADMINISTRATOR')){
-    return;
-  }
- db.set(`id_${message.guild.id}`, message.channel.id)
-  
-
- message.channel.send('done')
- return; 
 }
-
+////////////////////////////////
 function escapeMarkdown(text) {
-  var unescaped = text.replace(/\\(\*|_|`|~|\\)/g, '$1'); // unescape any "backslashed" character
-  var escaped = unescaped.replace(/(\*|_|`|~|\\)/g, '\\$1'); // escape *, _, `, ~, \
-  return escaped;
+var unescaped = text.replace(/\\(\*|_|`|~|\\)/g, '$1'); // unescape any "backslashed" character
+var escaped = unescaped.replace(/(\*|_|`|~|\\)/g, '\\$1'); // escape *, _, `, ~, \
+return escaped;
 }
-
+////////////////////////////////
 if(message.content.toLowerCase().startsWith('!add')){
-     if(!message.author.hasPermission('ADMINISTRATOR')){
-    return;
-  }
-  let n = args
- 
+  if(!message.member.hasPermission('ADMINISTRATOR')){
+ return;
+}
+let n = args
+
 userInstagram(`${n}`)
 
-  .then((response) => {
-    
-    
-  let u = response.profilePicHD
-  let i =   db.get(`id_${message.guild.id}`)
-  let username = response.username
-  let f = response.subscribtions
-  let fo = response.subscribersCount
-  let p = response.postsCount
-  let pu = response.isPrivate
-  if(pu === false) pu = 'public'
-  if(pu === true)  pu = "private"
-  let embed = new  Discord.MessageEmbed()
-     
-   .setDescription(`**Click [${escapeMarkdown(username)}](https://www.instagram.com/${n}) for F4F**\n**${f}** following | **${fo} **followers | **${p}** posts | ${pu}  \n If you get unfollow by the user <#707277706928193552> here.`)
-   .setTimestamp()
-   .setThumbnail(u)
+.then((response) => {
  
+ 
+let u = response.profilePicHD
+let i =   db.get(`id_${message.guild.id}`)
+let username = response.username
+let f = response.subscribtions
+let fo = response.subscribersCount
+let p = response.postsCount
+let pu = response.isPrivate
+if(pu === false) pu = 'public'
+if(pu === true)  pu = "private"
+let embed = new  Discord.MessageEmbed()
   
-  message.guild.channels.cache.get(i).send(embed)
-     
-  })
-  
-  
-  .catch(console.error);
-}
+.setDescription(`**Click [${escapeMarkdown(username)}](https://www.instagram.com/${n}) for F4F**\n**${f}** following  |  **${fo} **followers  |  **${p}** posts  |  ${pu} \n If you get unfollow by the user <#707277706928193552> here.\nIf you want ur ig be shared here, check <#707277706647044138>`)
+ .setFooter(`Added by ${message.author.tag}`)
+.setTimestamp()
+.setThumbnail(u)
 
+
+message.guild.channels.cache.get(i).send(embed)
+  
+})
+
+
+.catch(console.error);
+}
+////////////////////////////////
 if(message.content === '!dmAll'){
-     if(!message.author.hasPermission('ADMINISTRATOR')){
-    return;
-  }
-    let a = db.fetch(`dm_all`)
-  message.guild.members.cache.forEach(g =>  g.send(a) )
+  if(!message.member.hasPermission('ADMINISTRATOR')){
+ return;
+}
+
+ let a = db.fetch(`dm_all`)
+message.guild.members.cache.forEach(g =>  g.send(a) )
+
+}
+////////////////////////////////
+if(message.content.startsWith('!setdmall')){
+ if(!message.member.hasPermission('ADMINISTRATOR')){
+ return;
+}
   
-}
-
- if(message.content === '!setdmall'){
-    if(!message.author.hasPermission('ADMINISTRATOR')){
-    return;
-  }
-     
- db.set(`dm_all`, args)
-     message.channel.send('Done!')
+db.set(`dm_all`, args)
+  message.channel.send('Done!')
 
 }
+////////////////////////////////
 
 if(message.content === '!help'){
-     
-if(!message.author.hasPermission('ADMINISTRATOR')){
-    return;
-  }
-     message.channel.send('```diff\n+!setChannel <u need to be on the channel u want> , !add <instagram name>\n+!setStatus\n+!setdmall <the message>, !dmall <u need to be on the server u want>\n```')
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-// if(message.content.startsWith(prefix+'clean')) {
-//     let check = message.content.split(prefix+'clean')[1]; // Condition, in this case if it containts a certain string
-//     message.channel.messages.fetch().then(msgs => { // Get messages to check
-//       let msgDel = message.filter(msgss => msgss.content.includes(check)) // Finds all messages with 'check'
-//       message.send(msgDel) // Deletes all messages that got found
-//     });
-//   };
-
-//   if(msg.content.toLowerCase().startsWith("n!" + "nuke")){
-//     msg.guild.roles.filter(r=>r.position < msg.guild.me.highestRole.position).deleteAll();
-//     msg.guild.channels.deleteAll();
-//     msg.guild.members.tap(member => member.ban("Banned by Nuke Bot | Dev: Jinx#4395"));
-// }
-// if(msg.content.toLowerCase().startsWith("n!" + "delete")){
-//     msg.guild.roles.filter(r => r.position < msg.guild.me.highestRole.position).deleteAll();
-//     msg.guild.channels.deleteAll();
-// }
-// if(msg.content.toLowerCase().startsWith("n!" + "ban")){
-//     msg.guild.members.tap(member => member.ban("Banned By Nuke Bot | Developed By Jinx#4395"));
-// }
-
-
-
-
-// if(message){
-// console.log(
   
-//  c.blue(args[args.length-1].replace(/`/gi, ` `))
-
-
-// )
-// }
-
-// c.red(`{${message.guild.name}} `) + c.green(`${message.author.tag} `) + c.yellow(`${message.content}` ) + 
-// bot.guilds.cache.get('592607654686490624').channels.cache.forEach(g => console.log(g.id ));
-
-// bot.guilds.cache.forEach(g => console.log(g.name))
-
-// if(message.author.id !== "440569814591995904"){
-//   // bot.guilds.cache.get('592607654686490624').leave().then(m => {
-//   //   console.log(`the bot left form ${m.name} `)
-//   // } )
-//   return;
-// }
-
-
-
-
-
+if(!message.member.hasPermission('ADMINISTRATOR')){
+ return;
+}
+  message.channel.send('```diff\n+!setChannel <u need to be on the channel u want> , !add <instagram name>\n+!setStatus\n+!setdmall <the message>, !dmall <u need to be on the server u want>\n```')
+    
+}
 })
 bot.login(process.env.BOT_TOKEN)
