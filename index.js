5111a462ef39604036744194aa03fa452e124980
arjellel <52532423+arjellel@users.noreply.github.com>
2021-03-16 20:10:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -132,7 +132,8 @@ async function execute(client) {
     let song = queue.songs[i]
     if(i === queue.songs.length-1){
       console.log(0)
-      client.leaveVoiceChannel(queue.voiceChannelID);
+      client.guilds.cache.get(queue.guildID).channels.cache.get(queue.voiceChannelID).leave()
+      client.guilds.cache.get(queue.guildID).channels.cache.get(queue.textChannelID).send("leaving")
       console.log("Leaving")
       connecting(client, queue, 0)     
     }
